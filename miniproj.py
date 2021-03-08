648eef0d2409504a98bfb501b232637b399e186d
sittu <mithileshd333@gmail.com>
2021-03-08 21:09:04 +0530

    hope2

--- miniproj.py
+++ miniproj.py
@@ -91,7 +91,7 @@ def fun(stri):
         # get the snippet, statistics & content details from the video response
         #snippet = items["snippet"]
         # get infos from the snippet
-       # channel_title = snippet["channelTitle"]
+        # channel_title = snippet["channelTitle"]
         #title = snippet["title"]
         #description = snippet["description"]
 
@@ -110,8 +110,41 @@ def fun(stri):
         Categories_JSON = pd.read_json("US_category_id.JSON")
         Categories_JSON.head(3)
 
-        CategoryDict = [{'id': item['id'], 'title': item['snippet']['title']} for item in Categories_JSON['items']]
-
+        global Categories    
+        global CategoryDict
+        #CategoryDict = [{'id': item['id'], 'title': item['snippet']['title']} for item in Categories_JSON['items']]
+        CategoryDict = [{'id': '1', 'title': 'Film & Animation'},
+        {'id': '2', 'title': 'Autos & Vehicles'},
+        {'id': '10', 'title': 'Music'},
+        {'id': '15', 'title': 'Pets & Animals'},
+        {'id': '17', 'title': 'Sports'},
+        {'id': '18', 'title': 'Short Movies'},
+        {'id': '19', 'title': 'Travel & Events'},
+        {'id': '20', 'title': 'Gaming'},
+        {'id': '21', 'title': 'Videoblogging'},
+        {'id': '22', 'title': 'People & Blogs'},
+        {'id': '23', 'title': 'Comedy'},
+        {'id': '24', 'title': 'Entertainment'},
+        {'id': '25', 'title': 'News & Politics'},
+        {'id': '26', 'title': 'Howto & Style'},
+        {'id': '27', 'title': 'Education'},
+        {'id': '28', 'title': 'Science & Technology'},
+        {'id': '29', 'title': 'Nonprofits & Activism'},
+        {'id': '30', 'title': 'Movies'},
+        {'id': '31', 'title': 'Anime/Animation'},
+        {'id': '32', 'title': 'Action/Adventure'},
+        {'id': '33', 'title': 'Classics'},
+        {'id': '34', 'title': 'Comedy'},
+        {'id': '35', 'title': 'Documentary'},
+        {'id': '36', 'title': 'Drama'},
+        {'id': '37', 'title': 'Family'},
+        {'id': '38', 'title': 'Foreign'},
+        {'id': '39', 'title': 'Horror'},
+        {'id': '40', 'title': 'Sci-Fi/Fantasy'},
+        {'id': '41', 'title': 'Thriller'},
+        {'id': '42', 'title': 'Shorts'},
+        {'id': '43', 'title': 'Shows'},
+        {'id': '44', 'title': 'Trailers'}]
         CategoriesDF = pd.DataFrame(CategoryDict)
         Categories = CategoriesDF.rename(index=str, columns={"id": "Category_ID", "title": "Category"})
         Categories.head(3)
@@ -160,6 +193,8 @@ def fun(stri):
         cols = FinalDF.columns.tolist()
         cols = cols[-1:] + cols[:-1]
         FinalDF = FinalDF[cols]
+        Categories
+        CategoryDict
 
         return FinalDF
 
